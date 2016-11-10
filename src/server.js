8eb0bd5f67e607e67f5047333246d81880a45bce
jeremybell <jb@jeremybell.com>
2016-11-10 17:31:54 -0500

    changed bot message handling

--- src/server.js
+++ src/server.js
@@ -25,13 +25,13 @@ bot.on('message', (payload, reply) => {
 
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err
-  })
 
-  text = "Hi " + profile.first_name + ". You Said: " + text
+      text = "Hi " + profile.first_name + ". You Said: " + text
 
-  reply({ text }, (err) => {
-    if (err) throw err
-    // console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    reply({ text }, (err) => {
+      if (err) throw err
+      // console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
   })
 })
 
